0dd549d0a0f2bc42c8fd816175f6a2588d4ca666
damien <contact.damiendiaz@gmail.com>
2023-10-05 19:28:07 +0200

    show api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
-  fetch('./db.js')
+  const dbJson = "http://localhost:3000/products";
+  console.log(dbJson);
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    console.log("start in useEffect");
+    fetch(dbJson) //GET //recupération de donnees
+      .then((response) => response.json()) //traitement des donnees
+      .then((data) => {
+        setProducts(data);
+      }) //explotation des donnees
+      .catch((err) => {
+        //recupération de l'erreur
+        console.log(err);
+      });
+  }, []);
+
+  
+  const deleteProduct = async (id) => {
+    console.log('delete', id)
+
+    fetch(`http://localhost:3000/products/${id}`, {
+      method:'DELETE'}
+      )
+      .then(re =>console.log('suppression réussie'))
+      .catch(err => console.log(err))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {products.map((product) => (<ul>
+          <li key={product.id}> 
+            <p>
+              {product.title}<br/>
+              {product.brand}
+              </p>
+              <img src={product.thumbnail} alt="img du produit"></img>
+              <button onClick={()=>{deleteProduct(product.id)}}>x</button>
+            </li>           
+        </ul>)
+    )}
+
     </div>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,3 +1,19 @@
-export default function Product(){
-    
-}
+import deleteProduct from "../App";
+
+function Produits ({ produit }, { deleteProduct }) {
+    return (
+        <div>
+        <ul>
+          <li key={produit.id}> 
+            <p>
+              {produit.title}<br/>
+              {produit.brand}
+              </p>
+              <img src={produit.thumbnail} alt="img du produit"></img>
+              <button onClick={deleteProduct}>x</button>
+            </li>           
+        </ul>
+      </div>
+    );
+} 
+export default Produits;
